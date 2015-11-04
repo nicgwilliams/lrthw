f798a69477a9621a0571c843ea790dea4d6bf3da
nicgwilliams <nicgwilliams1@gmail.com>
2015-11-04 13:39:42 -0600

    added through ex 6

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+height_cent = height * 2.54
+weight = 180 # lbs
+weight_kilos = weight * 0.453592
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+
+
+puts "Let's talk about #{name}."
+puts "He's #{height_cent} centimeters tall."
+puts "He's #{weight_kilos} kilos heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
