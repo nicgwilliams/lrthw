807a6d2237e483a7a5855d474a0308e414f05a6d
nicgwilliams <nicgwilliams1@gmail.com>
2015-11-05 16:27:26 -0600

    More files

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food\a
+\t* Fishies\a
+\t* Catnip\n\t* Grass\a
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
---
+++ ex9.rb
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+1
+  2
+    3
+      4
+        5
+          6
+"""
