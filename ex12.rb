c8e05b1afec6a5c8a343378f541531d4dff185bf
nicgwilliams <nicgwilliams1@gmail.com>
2015-11-05 22:07:01 -0600

    more files

---
+++ ex12.rb
@@ -0,0 +1,17 @@
+#print "Give me a number: "
+#number = gets.chomp.to_f
+#
+#bigger = number * 100
+#puts "A bigger number is #{bigger}."
+#
+#print "Give me another number: "
+#another = gets.chomp
+#number = another.to_f
+#
+#smaller = number / 100
+#puts "A smaller number is #{smaller}."
+
+print "Give me your money! "
+money = gets.chomp.to_f
+ten_percent = money * 0.1
+print "You can have #{ten_percent} back.\n"
